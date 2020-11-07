3cbdfd10edcd5bb802773ca11f9669b488d35e52
moyamong <daniel123800@gmail.com>
2020-11-07 17:03:22 +0900

    bee movement update

--- Assets/Scripts/WorkerBee.cs
+++ Assets/Scripts/WorkerBee.cs
@@ -8,9 +8,12 @@ public class WorkerBee : Bee
     float speed = 5f;
     Vector3 arrivalPoint;
     bool isMoving;
+    SpriteRenderer renderer;
+
     void Start()
     {
         isMoving = false;
+        renderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -31,6 +34,35 @@ public class WorkerBee : Bee
             Vector3 direction = arrivalPoint - transform.position;
             direction = direction.normalized * speed;
             transform.Translate(direction * Time.deltaTime);
+            SpriteFlipping(direction);
+            //SpriteDirection(direction);
+        }
+    }
+
+    void SpriteFlipping(Vector3 direction)
+    {
+        if (direction.x > 0)
+        {
+            renderer.flipX = true;
+        }
+        else
+        {
+            renderer.flipX = false;
+        }
+    }
+
+    void SpriteDirection(Vector3 direction)
+    {
+        if (direction.x > 0)
+        {
+            renderer.flipX = true;
+            renderer.
+            transform.rotation = Quaternion.AngleAxis(180 / Mathf.PI * Mathf.Atan2(direction.y, Mathf.Abs(direction.x)),Vector3.forward);
+        }
+        else
+        {
+            renderer.flipX = false;
+            transform.rotation = Quaternion.AngleAxis(Mathf.Atan2(direction.y, Mathf.Abs(direction.x)), Vector3.forward);
         }
     }
 
