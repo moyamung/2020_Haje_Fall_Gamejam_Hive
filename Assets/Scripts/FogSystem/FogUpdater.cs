62a9be888cca2ebcbfc0139d9c9e0ae16a988d13
moyamong <daniel123800@gmail.com>
2020-11-08 17:21:59 +0900

    Fog of War Merging

---
+++ Assets/Scripts/FogSystem/FogUpdater.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogUpdater : MonoBehaviour
+{
+    public FogTexture[] fogs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fogs = FindObjectsOfType<FogTexture>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var fog in fogs) {
+            fog.UpdateAt(transform.position, 4);
+        }
+    }
+}
---
+++ Assets/Scripts/FogSystem/SyncCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyncCamera : MonoBehaviour
+{
+    public Camera target;
+    private Camera cam;
+    
+    private void Start() {
+        cam = GetComponent<Camera>();
+    }
+
+    private void OnPreRender() {
+        cam.orthographicSize = target.orthographicSize;
+    }
+}
